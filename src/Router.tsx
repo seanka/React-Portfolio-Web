667fce51b2408b1b69e11d610e4fdfe6d3f1ba09
Sean Anderson <seankristian16@gmail.com>
2024-06-10 11:41:48 +0700

    core: - Fixed project name typo - Added setup for github pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,17 +5,20 @@ import { RouteNavigation } from "./Common/Enum/RouteNavigation";
 import Root from "./Presentation/Common/Root";
 import PortfolioViewController from "./Presentation/Portfolio/ViewController/PortfolioViewController";
 
-const Router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: RouteNavigation.portfolio,
-        element: <PortfolioViewController />,
-      },
-    ],
-  },
-]);
+const Router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: RouteNavigation.portfolio,
+          element: <PortfolioViewController />,
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
 
 export default Router;
