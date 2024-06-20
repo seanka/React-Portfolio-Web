d5b70ecc51497e95f2134f99ce5a625a1c0e31c0
Sean Anderson <seankristian16@gmail.com>
2024-06-20 19:49:48 +0700

    portfolio: - Added logic to persist decorations coordinates - Removed storage on page refresh

--- src/Common/Interface/ImageProperty.ts
+++ src/Common/Interface/ImageProperty.ts
@@ -1,5 +1,8 @@
+import { Dimension } from "./Dimension";
+
 export interface ImageProperty {
   image: string;
   alt: string;
   hover?: string;
+  dimension?: Dimension;
 }
--- src/Presentation/Common/Root.tsx
+++ src/Presentation/Common/Root.tsx
@@ -1,8 +1,21 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useEffect } from "react";
+
+import { usePageRefresh } from "../../Common/Utils/usePageRefresh";
 
 import { RouteNavigation } from "../../Common/Enum/RouteNavigation";
+import { PortfolioKey } from "../../Common/Enum/LocalData/PortfolioKey";
 
 function Root() {
+  const isPageReloaded = usePageRefresh();
+
+  useEffect(() => {
+    if (isPageReloaded) {
+      localStorage.removeItem(PortfolioKey.birdDecoCoordinates);
+      localStorage.removeItem(PortfolioKey.cloudDecoCoordinates);
+    }
+  }, [isPageReloaded]);
+
   return (
     <>
       <Navigate to={RouteNavigation.portfolio} />
