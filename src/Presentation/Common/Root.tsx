0f505a56955d3e465f06a8afd875fcc7d4caa58d
Sean Anderson <seankristian16@gmail.com>
2025-02-05 16:03:59 +0700

    Core :: Add SFPro custom fonts

--- src/Presentation/Common/Root.tsx
+++ src/Presentation/Common/Root.tsx
@@ -20,7 +20,17 @@ function Root() {
   return (
     <>
       <Navigate to={RouteNavigation.portfolio} />
-      <Text className="text-9xl font-bold underline">HALO</Text>
+      <Text className="font-sfpro text-3xl underline">HALO</Text>
+      <Text className="text-3xl underline">HALO</Text>
+      <Text
+        style={{ fontFamily: "SFPro" }}
+        className="font-sfpro text-3xl underline"
+      >
+        the quick brown fox jumps over the lazy dog
+      </Text>
+      <Text className="text-3xl underline">
+        the quick brown fox jumps over the lazy dog
+      </Text>
       <Outlet />
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
+import "../index.css";
 import Router from "./Router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ChakraProvider>
     <RouterProvider router={Router}></RouterProvider>
-  </ChakraProvider>
+  </ChakraProvider>,
 );
